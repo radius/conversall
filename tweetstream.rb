d40675ce615ada56645c85b3d4edfd70652915cb
darius daftary <dariusdaftary@gmail.com>
2012-10-15 02:25:26 -0400

    inital commit

---
+++ tweetstream.rb
@@ -0,0 +1,9 @@
+require 'tweetstream'
+
+TweetStream.configure do |config|
+  config.consumer_key       = '1SlFlzJzjwFb8Hu0Xg7gQ'
+  config.consumer_secret    = 'd98spifmqqD4PDC9A228jH6qfNDNmxPrhaM7eEdR7A8'
+  config.oauth_token        = '5939272-q771BC0UaTMTKUICrBRn5ueJOwvxvuENox1YyEqoyM'
+  config.oauth_token_secret = 'CzZYGuU1xwOROkYT8X2v3rlpLjaNZFJHRFjQDXQp5w'
+  config.auth_method        = :oauth
+end
